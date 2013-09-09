7b255c8777556ade13a03bf56feacb2b30bff2e7
prajan <prajan.shrestha@gmail.com>
2013-09-09 14:03:51 +0200

    Updated

--- Individuals/week1_PS/week1.hs
+++ Individuals/week1_PS/week1.hs
@@ -1,4 +1,35 @@
-doubleMe x = x + x
+
+double :: Int -> Int
+double n = 2 * n
 
 
-// 1.1 precedene : ( ), / , * , + , -  [ BODMAS ] 
+
+sentence = "Sentences can go " ++ onAndOn
+onAndOn = "on and " ++ onAndOn
+
+
+sentenceMap = "Sentence can go " :
+                               map (++ " and on ") sentenceMap
+
+
+
+
+
+{- Lambda Abstraction In Haskel -}
+sqr :: Int -> Int
+sqr = \x -> x * x
+
+
+{- Lambda abstractions and concatenation -}
+name = ( \x      ->    x ++ "Shrestha" ) "Prajan "
+
+
+reversal :: [a] -> [a]   {- [a] is  list over a -}
+{-reversal [] = [] -}  
+reversal (x : t) = reversal  t  ++   [x]      {- the head is x and tail is t for reversal -}
+
+
+
+
+{- | 1.1 precedene : ( ), / , * , + , -  [ BODMAS ]     -}
+
