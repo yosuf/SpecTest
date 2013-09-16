c08c66386b1952bb55895aa83d7359d778439189
Yosuf <yosuf.haydary@gmail.com>
2013-09-16 20:46:43 +0200

    Rectangular test

--- Assignments/week2/assignment1.hs
+++ Assignments/week2/assignment1.hs
@@ -1,20 +1,42 @@
 module Lab2
 where
 
--- Question 1.
--- Triangles
+import Data.List
+
+
+
 data Shape = NoTriangle | Equilateral | Isosceles | Rectangular | Other deriving (Eq, Show)
 
+-- Yosuf
 triangle :: Integer -> Integer -> Integer -> Shape
-triangle x y z
-	| x<1 || y<1 || z<1 = NoTriangle   -- like: 0 1 1
-	| x == y && y == z = Equilateral		-- like: 5 5 5 
-	| x == y || x == z || y == z = Isosceles  -- like: 2 2 6
-	| (min x y)^2 + (min y z)^2 == (max (max x y) z)^2 = Rectangular -- like: 3 4 5
-	| otherwise = Other -- like anything else
+triangle x y z = triangle' (sort [x,y,z] )
+
+-- pre-condition: Sorted list of integers which has max 3 elements. 
+triangle' :: [Integer] -> Shape
+triangle' (x:y:z:rest)
+  | length rest /= 0 = NoTriangle
+  | x<0 || y<0 || z<0 = NoTriangle
+  | x + y <= z = NoTriangle
+  | x == y && y == z = Equilateral
+  | x == y || x == z || y == z = Isosceles
+  | x^2 + y^2 == z^2 = Rectangular
+  | otherwise = Other
 
   
- 
+-- According to input, this method generates the possible rectangulars from a given range
+-- exampe: generateRect [1..50] will result in a list of lists [[3,4,5], ...] 
+-- which can then be used to chech the program
+generateRect :: [Integer] -> [[Integer]]
+generateRect xs = [[x,y,z] | x <- xs, y <- [1..x-1], z <- [1..x-1] , x^2 == y^2 + z^2 ]
+
+-- This is the ultimate test case for the rectangular triangles ;)
+-- How does it work? 
+-- Invoke it with a numer. it will call the generateRect to generate valid rect triangles.
+-- It then 'asserts' theses to the triangle method and notes whether the outcome is Rectangular
+testRect :: Integer -> [Bool]
+testRect nrOfTestCases = [ triangle x y z == Rectangular | x:y:z:testSet <- generateRect[1..nrOfTestCases] ]
+
+
 
 -- Jeroen:
 
@@ -30,7 +52,7 @@ is_isosceles a b c = or [a1 == b1 && not (a1 == c1) | a1 <- [a, b, c],
                                                       b1 <- [a, b, c],
                                                       c1 <- [a, b, c]]
 
-triangle' a b c  | not (is_triangle a b c) = NoTriangle
+triangle'' a b c  | not (is_triangle a b c) = NoTriangle
                 | is_equalateral a b c = Equilateral
                 | is_rectangular a b c = Rectangular
                 | is_isosceles a b c = Isosceles
@@ -41,4 +63,4 @@ assert :: Shape -> Shape -> Shape
 assert expected actual  | expected == actual = expected
                         | otherwise = error "expected not the same as actual"
 
-prop1 = assert (triangle' 10 5 5)
+prop1 = assert (triangle'' 10 5 5)
--- Individuals/week2_YH/Triangles.hs
+++ Individuals/week2_YH/Triangles.hs
@@ -1,15 +1,41 @@
 module Triangles where
 
+import Test.QuickCheck
+import Data.List
 
 data Shape = NoTriangle | Equilateral | Isosceles | Rectangular | Other deriving (Eq, Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
-triangle x y z
-	| x<1 || y<1 || z<1 = NoTriangle   -- like: 0 1 1
-	| x == y && y == z = Equilateral		-- like: 5 5 5 
-	| x == y || x == z || y == z = Isosceles  -- like: 2 2 6
-	| (min x y)^2 + (min y z)^2 == (max (max x y) z)^2 = Rectangular -- like: 3 4 5
-	| otherwise = Other -- like anything else
+triangle x y z = triangle' (sort [x,y,z] )
+
+-- pre-condition: Sorted list of integers which has max 3 elements. 
+triangle' :: [Integer] -> Shape
+triangle' (x:y:z:rest)
+	| length rest /= 0 = NoTriangle
+	| x<0 || y<0 || z<0 = NoTriangle
+	| x + y <= z = NoTriangle
+	| x == y && y == z = Equilateral
+	| x == y || x == z || y == z = Isosceles
+	| x^2 + y^2 == z^2 = Rectangular
+	| otherwise = Other
+
+
+
+generateRect :: [Integer] -> [[Integer]]
+generateRect xs = [[x,y,z] | x <- xs, y <- [1..x-1], z <- [1..x-1] , x^2 == y^2 + z^2 ]
+
+testRect :: Integer -> [Bool]
+testRect nrOfTestCases = [ triangle x y z == Rectangular | x:y:z:testSet <- generateRect[1..nrOfTestCases] ]
+
+--generateRect:: Integer -> Bool
+--generateRect x  = (div x 2)^2 + (div x 2)^2 == x^2  then True  else False
+
+
+--rectangularNr:: [Integer] -> [Integer]
+--rectangularNr	[] = []
+--rectangularNr (x:xs) = if ((div x 2)^2 + (div x 2)^2 == x^2) then x:(rectangularNr xs) else rectangularNr xs
+
+
 
 
 
@@ -18,3 +44,14 @@ triangle x y z
 Time spent: ~2 hours
 Tested: by running examples from ghci.
 -}
+
+test_Triangle_No x y z = triangle x y z == NoTriangle
+	where types = (x, y, z )
+
+-- successful
+prop_RevRev xs = reverse (reverse xs) == xs
+  where types = xs::[Integer]
+
+-- unsuccessful
+prop_RevId xs = reverse xs == xs
+  where types = xs::[Int]
