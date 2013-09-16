701480a15297c486707fae0dcf9a67e47234304f
Yosuf <yosuf.haydary@gmail.com>
2013-09-16 21:27:24 +0200

    Test cases for the triangles

--- Assignments/week2/assignment1.hs
+++ Assignments/week2/assignment1.hs
@@ -7,7 +7,11 @@ import Data.List
 
 data Shape = NoTriangle | Equilateral | Isosceles | Rectangular | Other deriving (Eq, Show)
 
--- Yosuf
+{- Report Yosuf Haydary
+Time spent: ~5 hours
+Tested by writing test functions which generates different triangles for different forms
+-}
+
 triangle :: Integer -> Integer -> Integer -> Shape
 triangle x y z = triangle' (sort [x,y,z] )
 
@@ -36,6 +40,36 @@ generateRect xs = [[x,y,z] | x <- xs, y <- [1..x-1], z <- [1..x-1] , x^2 == y^2 
 testRect :: Integer -> [Bool]
 testRect nrOfTestCases = [ triangle x y z == Rectangular | x:y:z:testSet <- generateRect[1..nrOfTestCases] ]
 
+
+-- Testing No Triangle
+generateNoTri :: [Integer] -> [[Integer]]
+generateNoTri xs = [[x,y,z] | x <- xs, y <- [0..x], z <- [x*2..x*2] ]
+
+testNoTri :: Integer -> [Bool]
+testNoTri nrOfTestCases = [ triangle x y z == NoTriangle | x:y:z:testSet <- generateNoTri[1..nrOfTestCases] ]
+
+
+-- Tesging Equilateral
+generateEqui :: [Integer] -> [[Integer]]
+generateEqui xs = [[x,x,x] | x <- xs ]
+
+testEqui :: Integer -> [Bool]
+testEqui nrOfTestCases = [ triangle x y z == Equilateral | x:y:z:testSet <- generateEqui[1..nrOfTestCases] ]
+
+
+-- Tesging Isoscelese
+generateIsos :: [Integer] -> [[Integer]]
+generateIsos xs = [[x,y,z] | x <- xs, y <- [1..x],  z <- [1..x], y==x, z/=x ]
+
+testIsos :: Integer -> [Bool]
+testIsos nrOfTestCases = [ triangle x y z == Isosceles | x:y:z:testSet <- generateIsos[1..nrOfTestCases] ]
+
+-- Testing Other
+generateOther :: [Integer] -> [[Integer]]
+generateOther xs = [[x,y,z] | x <- xs, y <- [1..x],  z <- [x..x*2], y/=x, y/=z, z/=x, x^2 /= y^2 + z^2, z < (x+y) ]
+
+testOther :: Integer -> [Bool]
+testOther nrOfTestCases = [ triangle x y z == Other | x:y:z:testSet <- generateOther[1..nrOfTestCases] ]
 
 
 -- Jeroen:
--- Individuals/week2_YH/Triangles.hs
+++ Individuals/week2_YH/Triangles.hs
@@ -21,28 +21,48 @@ triangle' (x:y:z:rest)
 
 
 
-generateRect :: [Integer] -> [[Integer]]
-generateRect xs = [[x,y,z] | x <- xs, y <- [1..x-1], z <- [1..x-1] , x^2 == y^2 + z^2 ]
+-- Testing No Triangle
+generateNoTri :: [Integer] -> [[Integer]]
+generateNoTri xs = [[x,y,z] | x <- xs, y <- [0..x], z <- [x*2..x*2] ]
 
+testNoTri :: Integer -> [Bool]
+testNoTri nrOfTestCases = [ triangle x y z == NoTriangle | x:y:z:testSet <- generateNoTri[1..nrOfTestCases] ]
+
+
+-- Tesging Equilateral
+generateEqui :: [Integer] -> [[Integer]]
+generateEqui xs = [[x,x,x] | x <- xs ]
+
+testEqui :: Integer -> [Bool]
+testEqui nrOfTestCases = [ triangle x y z == Equilateral | x:y:z:testSet <- generateEqui[1..nrOfTestCases] ]
+
+
+-- Tesging Equilateral
+generateIsos :: [Integer] -> [[Integer]]
+generateIsos xs = [[x,y,z] | x <- xs, y <- [1..x],  z <- [1..x], y==x, z/=x ]
+
+testIsos :: Integer -> [Bool]
+testIsos nrOfTestCases = [ triangle x y z == Isosceles | x:y:z:testSet <- generateIsos[1..nrOfTestCases] ]
+
+-- Testing Other
+generateOther :: [Integer] -> [[Integer]]
+generateOther xs = [[x,y,z] | x <- xs, y <- [1..x],  z <- [x..x*2], y/=x, y/=z, z/=x, x^2 /= y^2 + z^2, z < (x+y) ]
+
+testOther :: Integer -> [Bool]
+testOther nrOfTestCases = [ triangle x y z == Other | x:y:z:testSet <- generateOther[1..nrOfTestCases] ]
+
+
+-- Testing Rectangular
 testRect :: Integer -> [Bool]
 testRect nrOfTestCases = [ triangle x y z == Rectangular | x:y:z:testSet <- generateRect[1..nrOfTestCases] ]
 
---generateRect:: Integer -> Bool
---generateRect x  = (div x 2)^2 + (div x 2)^2 == x^2  then True  else False
-
-
---rectangularNr:: [Integer] -> [Integer]
---rectangularNr	[] = []
---rectangularNr (x:xs) = if ((div x 2)^2 + (div x 2)^2 == x^2) then x:(rectangularNr xs) else rectangularNr xs
-
-
-
-
+generateRect :: [Integer] -> [[Integer]]
+generateRect xs = [[x,y,z] | x <- xs, y <- [1..x-1], z <- [1..x-1] , x^2 == y^2 + z^2 ]
 
 
 {- Report
-Time spent: ~2 hours
-Tested: by running examples from ghci.
+Time spent: ~5 hours
+Tested by writing test functions which generates different triangles for different forms
 -}
 
 test_Triangle_No x y z = triangle x y z == NoTriangle
