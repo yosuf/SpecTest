426d45254b31aa98d615daa95705e1d89bb3e5f2
Khosrow-Azizi <khosrowazizi@live.nl>
2013-09-16 14:59:44 +0200

    added tests for ass1

--- Individuals/week2_KA/week2Lab.hs
+++ Individuals/week2_KA/week2Lab.hs
@@ -26,3 +26,10 @@ isRectangular a b c = isTriangle a b c  &&
                       (a^2 == b^2 + c^2 || 
 					   b^2 == a^2 + c^2 ||
 					   c^2 == a^2 + b^2)
+					   
+testTriangle :: Shape -> (Integer -> Integer -> Integer -> Bool) -> Bool
+testTriangle shape f = and [ (triangle a b c) == shape | c <- [1..10] , b <- [1.. c], a <-[1.. b], (f a b c)]					   
+--testIsEquilateral = and [ (triangle a b c) == Equilateral | c <- [1..10] , b <- [1.. c], a <-[1.. b], a < b + c && b < a + c && c < a + b &&(a == b && c == b) ] 
+testIsEquilateral = testTriangle Equilateral (\ a b c -> a < b + c && b < a + c && c < a + b &&(a == b && c == b))
+testIsIsosceles = testTriangle Isosceles (\ a b c -> a < b + c && b < a + c && c < a + b && (a == b || a == c || b == c) && not (a == b && c == b))
+testIsRectangular = testTriangle Rectangular (\ a b c -> a < b + c && b < a + c && c < a + b && (a^2 == b^2 + c^2 || b^2 == a^2 + c^2 || c^2 == a^2 + b^2))
