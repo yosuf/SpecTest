cbbcb741b2567207cddc233ae207af6173ee509e
Yosuf <yosuf.haydary@gmail.com>
2013-09-30 13:43:16 +0200

    Committing final W3A4 and A5

--- Assignments/week3/Assignment4_5.hs
+++ Assignments/week3/Assignment4_5.hs
@@ -34,41 +34,6 @@ testCount = [ 	0 == count 1 [],
 				1 == count 2 [9,2,3],
 				4 == count 0 [9,0,3,0,5,0,1,0] ]	
 
-
-{-Demi-}
-isPermutation'' :: Eq a => [a] -> [a] -> Bool
-isPermutation'' xs ys | length xs /= length ys = False
-                    | xs == ys = True
-                    | otherwise = or (map (==xs) (permutations ys))
-
-{-Prajan -}
-isPermutation' :: Eq a => [a] => [a] -> Bool   {- isPermutation [2,1,3] [1,2,3] : Reuslt is True-}
-isPermutation' [] [] = True
-isPermutation' (x:xs) ys | (length (x:xs) /= length ys) = error "List parameters not equal "
-                        |  x `elem` ys = isPermutation xs (delArgInList (==x) ys)
-                        |  otherwise = False
-
-delArgInList ::  (a -> Bool) -> [a] -> [a]   {- this is delete argument in list compare (== 1) [1,2,3,4]    : Result is [1,3,4]-}
-delArgInList p [] = []
-delArgInList p (x:xs) | p x      = xs
-                    | otherwise = x : delArgInList p xs
-
-{-Jeroen-}
-isPermutation'''' :: Eq a => [a] -> [a] -> Bool
-isPermutation'''' [] []     = True
-isPermutation'''' ys xs     | length ys /= length xs        = False
-                        | (length.removeAll ys) xs == 0 = True
-                        | otherwise = False
-
-removeAll [] xs = xs
-removeAll (y:ys) xs = removeAll ys (removeFst y xs)
-                        
-removeFst _ [] = []
-removeFst y (x:xs)  | y == x = xs
-                    | otherwise = x:(removeFst y xs)
-
-
-
 {- Assignment 5:
 Define some testable properties for this function, and use your random
 generator for integer lists from Exercise 3 to test isPermutation.-}
