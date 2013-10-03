d913863d6ce401ca4f898c4a56e9fff67c18b7a7
prajan <prajan.shrestha@gmail.com>
2013-10-03 19:18:05 +0200

    Week 5 , question no 1 solved. Updated README.txt

--- Assignments/week5/README.md
+++ Assignments/week5/README.md
@@ -3,8 +3,8 @@ Week 5 Assignment
 
 Who is busy with what?
 
-- [ ] Assignment 1: 
-- [ ] Assignment 2: 
+- [ ] Assignment 1: Prajan (Status Complete)
+- [ ] Assignment 2: Prajan (On Process)
 - [ ] Assignment 3: 
 - [ ] Assignment 4: 
 - [ ] Assignment 5: 
--- Individuals/week5_PS/week5.hs
+++ Individuals/week5_PS/week5.hs
@@ -12,11 +12,25 @@ mergeSrt (x:xs) = merge [x] (mergeSrt xs)
 assert1 :: (a -> b -> Bool) -> (a -> b) -> a -> b 
 assert1 p f x = if p x (f x) then f x 
                 else error "assert1"
+
+assert2 ::  (a -> b -> c -> Bool) 
+             -> (a -> b -> c) -> a -> b -> c
+assert2 p f x y = 
+  if p x y (f x y) then f x y
+  else error "assert2"
+
+
+mergeA :: Ord a => [a] -> [a] -> [a]
+mergeA = assert2 sortedProp 
+            $ assert2 sublistProp merge
+
 -}
 
+sortList :: Ord a => [a] -> [a] -> Bool    {- Decided onthe fact that assert1 first input is (a -> b-> Bool) -}
+sortList xs ys = sorted xs ==> sorted ys
 
 mergeSrtA :: Ord a => [a] => [a]
-mergeSrtA = assert1 merge $ assert1 mergeSrtA
+mergeSrtA = assert1 sortList mergeSrt   {- sorList function has two inputs and return a bool -}
 
 
 
